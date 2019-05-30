2592a1846599c190442c84403b1950fd3a060856
Robert Lyons <nextdude@gmail.com>
2019-05-30 16:11:57 -0400

    add tests for percentage

---
+++ src/test/scala/io/epiphanous/flinkrunner/model/aggregate/PercentageTest.scala
@@ -0,0 +1,41 @@
+package io.epiphanous.flinkrunner.model.aggregate
+
+import java.time.Instant
+
+import com.typesafe.scalalogging.LazyLogging
+import org.scalatest.{FlatSpec, Matchers}
+import squants.time.{Seconds, Time}
+
+class PercentageTest extends FlatSpec with Matchers with LazyLogging {
+  behavior of "PercentageTest"
+
+  val p = Percentage(Time.name, Seconds.symbol, 86400d)
+
+  it should "update" in {
+    val q = p.update(86400 / 2, "s", Instant.now())
+    q.map(_.value) shouldBe Some(0.5)
+  }
+
+  it should "dimension" in {
+    p.dimension shouldBe "Time"
+  }
+
+  it should "value" in {
+    p.value shouldBe 0d
+  }
+
+  it should "baseParam" in {
+    p.baseParam shouldEqual 86400d
+  }
+
+  it should "params" in {
+    p.params.isEmpty shouldBe false
+    p.params.contains("base") shouldBe true
+    p.params.get("base") shouldBe Some(86400d)
+  }
+
+  it should "name" in {
+    p.name shouldBe "Percentage"
+  }
+
+}
