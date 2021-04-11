89fb621ef3873ba2d5a631c55bc96a0375232603
Robert Lyons <nextdude@gmail.com>
2021-04-11 18:38:27 -0400

    fix kinesis serdes

--- src/main/scala/io/epiphanous/flinkrunner/model/FlinkConfig.scala
+++ src/main/scala/io/epiphanous/flinkrunner/model/FlinkConfig.scala
@@ -4,7 +4,10 @@ import com.typesafe.config.{ConfigFactory, ConfigObject}
 import com.typesafe.scalalogging.LazyLogging
 import io.epiphanous.flinkrunner.{FlinkRunnerFactory, SEE}
 import org.apache.flink.api.java.utils.ParameterTool
-import org.apache.flink.contrib.streaming.state.{PredefinedOptions, RocksDBStateBackend}
+import org.apache.flink.contrib.streaming.state.{
+  PredefinedOptions,
+  RocksDBStateBackend
+}
 import org.apache.flink.runtime.state.filesystem.FsStateBackend
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
@@ -17,37 +20,43 @@ import scala.util.{Failure, Success, Try}
 
 @SerialVersionUID(1544548116L)
 class FlinkConfig(
-                   args: Array[String],
-                   factory: FlinkRunnerFactory[_],
-                   sources: Map[String, Seq[Array[Byte]]] = Map.empty,
-                   optConfig: Option[String] = None)
-  extends LazyLogging
+    args: Array[String],
+    factory: FlinkRunnerFactory[_],
+    sources: Map[String, Seq[Array[Byte]]] = Map.empty,
+    optConfig: Option[String] = None)
+    extends LazyLogging
     with Serializable {
 
   val (jobName, jobArgs, jobParams) = {
     val (n, a) = args match {
       case Array(opt, _*) if opt.startsWith("-") => ("help", args)
-      case Array("help", _*) => ("help", args.tail)
-      case Array(jn, "help", _*) => (jn, Array("--help") ++ args.tail)
-      case Array(jn, _*) => (jn, args.tail)
-      case _ => ("help", args)
+      case Array("help", _*)                     => ("help", args.tail)
+      case Array(jn, "help", _*)                 => (jn, Array("--help") ++ args.tail)
+      case Array(jn, _*)                         => (jn, args.tail)
+      case _                                     => ("help", args)
     }
     (n, a, ParameterTool.fromArgs(a))
   }
 
   val _config = {
-    val sc = Seq(ConfigFactory.load(), ConfigFactory.load("flink-runner.conf"))
+    val sc  =
+      Seq(ConfigFactory.load(), ConfigFactory.load("flink-runner.conf"))
     val ocf =
       if (jobParams.has("config"))
         Some(ConfigFactory.parseFile(new File(jobParams.get("config"))))
       else None
     val ocs = optConfig.map(ConfigFactory.parseString)
     // precedence in config is from right to left...
-    (ocs ++ ocf ++ sc).foldRight(ConfigFactory.empty())((z, c) => z.withFallback(c))
+    (ocs ++ ocf ++ sc).foldRight(ConfigFactory.empty())((z, c) =>
+      z.withFallback(c)
+    )
   }
 
   def getCollectionSource(name: String) =
-    sources.getOrElse(name, throw new RuntimeException(s"missing collection source $name"))
+    sources.getOrElse(
+      name,
+      throw new RuntimeException(s"missing collection source $name")
+    )
 
   val systemName = _config.getString("system.name")
 
@@ -77,43 +86,46 @@ class FlinkConfig(
   def getString(path: String): String =
     _s(path) match {
       case ("a", p) => jobParams.get(p)
-      case (_, p) => _config.getString(p)
+      case (_, p)   => _config.getString(p)
     }
 
   def getStringList(path: String): List[String] =
     _s(path) match {
       case ("a", p) => jobParams.get(p).split("[, ]+").toList
-      case (_, p) => _config.getStringList(p).asScala.toList
+      case (_, p)   => _config.getStringList(p).asScala.toList
     }
 
   def getInt(path: String): Int =
     _s(path) match {
       case ("a", p) => jobParams.getInt(p)
-      case (_, p) => _config.getInt(p)
+      case (_, p)   => _config.getInt(p)
     }
 
   def getLong(path: String): Long =
     _s(path) match {
       case ("a", p) => jobParams.getLong(p)
-      case (_, p) => _config.getLong(p)
+      case (_, p)   => _config.getLong(p)
     }
 
   def getBoolean(path: String): Boolean =
     _s(path) match {
       case ("a", p) => jobParams.getBoolean(p)
-      case (_, p) => _config.getBoolean(p)
+      case (_, p)   => _config.getBoolean(p)
     }
 
   def getDouble(path: String): Double =
     _s(path) match {
       case ("a", p) => jobParams.getDouble(p)
-      case (_, p) => _config.getDouble(p)
+      case (_, p)   => _config.getDouble(p)
     }
 
   def getDuration(path: String): Duration =
     _s(path) match {
-      case ("a", p) => ConfigFactory.parseString(s"$p = ${jobParams.get(p)}").getDuration(p)
-      case (_, p) => _config.getDuration(p)
+      case ("a", p) =>
+        ConfigFactory
+          .parseString(s"$p = ${jobParams.get(p)}")
+          .getDuration(p)
+      case (_, p)   => _config.getDuration(p)
     }
 
   def getProperties(path: String): Properties = {
@@ -122,46 +134,70 @@ class FlinkConfig(
     def flatten(key: String, value: Object): Unit = {
       val pkey = if (key.isEmpty) key else s"$key."
       value match {
-        case map: JMap[String, Object]@unchecked => map.asScala.foreach { case (k, v) => flatten(s"$pkey$k", v) }
-        case list: JList[Object]@unchecked =>
-          list.asScala.zipWithIndex.foreach { case (v, i) => flatten(s"$pkey$i", v) }
-        case v =>
+        case map: JMap[String, Object] @unchecked =>
+          map.asScala.foreach { case (k, v) => flatten(s"$pkey$k", v) }
+        case list: JList[Object] @unchecked       =>
+          list.asScala.zipWithIndex.foreach { case (v, i) =>
+            flatten(s"$pkey$i", v)
+          }
+        case v                                    =>
           p.put(key, v.toString)
           () // force unit return
       }
     }
 
     (_s(path) match {
-      case ("a", p) => Some(ConfigFactory.parseString(s"$p = ${jobParams.get(p)}").getObject(p))
-      case (_, p) => if (_config.hasPath(p)) Some(_config.getObject(p)) else None
+      case ("a", p) =>
+        Some(
+          ConfigFactory
+            .parseString(s"$p = ${jobParams.get(p)}")
+            .getObject(p)
+        )
+      case (_, p)   =>
+        if (_config.hasPath(p)) Some(_config.getObject(p)) else None
     }) match {
       case Some(c) => flatten("", c.unwrapped())
-      case None => // noop
+      case None    => // noop
     }
     p
   }
 
   def _classInstance[T](path: String): T =
-    Class.forName(getString(path)).getDeclaredConstructor().newInstance().asInstanceOf[T]
+    Class
+      .forName(getString(path))
+      .getDeclaredConstructor()
+      .newInstance()
+      .asInstanceOf[T]
 
   def getJobInstance = factory.getJobInstance(jobName)
 
-  def getDeserializationSchema(sourceConfig: SourceConfig) = factory.getDeserializationSchema(sourceConfig)
+  def getDeserializationSchema(sourceConfig: SourceConfig) =
+    factory.getDeserializationSchema(sourceConfig)
 
   def getKafkaDeserializationSchema(sourceConfig: KafkaSourceConfig) =
     factory.getKafkaDeserializationSchema(sourceConfig)
 
-  def getSerializationSchema(sinkConfig: SinkConfig) = factory.getSerializationSchema(sinkConfig)
+  def getKinesisDeserializationSchema(sourceConfig: KinesisSourceConfig) =
+    factory.getKinesisDeserializationSchema(sourceConfig)
 
-  def getKafkaSerializationSchema(sinkConfig: KafkaSinkConfig) = factory.getKafkaSerializationSchema(sinkConfig)
+  def getSerializationSchema(sinkConfig: SinkConfig) =
+    factory.getSerializationSchema(sinkConfig)
+
+  def getKafkaSerializationSchema(sinkConfig: KafkaSinkConfig) =
+    factory.getKafkaSerializationSchema(sinkConfig)
+
+  def getKinesisSerializationSchema(sinkConfig: KinesisSinkConfig) =
+    factory.getKinesisSerializationSchema(sinkConfig)
 
   def getEncoder(sinkConfig: SinkConfig) = factory.getEncoder(sinkConfig)
 
-  def getAddToJdbcBatchFunction(sinkConfig: SinkConfig) = factory.getAddToJdbcBatchFunction(sinkConfig)
+  def getAddToJdbcBatchFunction(sinkConfig: SinkConfig) =
+    factory.getAddToJdbcBatchFunction(sinkConfig)
 
   def getBucketAssigner(p: Properties) = factory.getBucketAssigner(p)
 
-  def getSourceConfig(name: String): SourceConfig = SourceConfig(name, this)
+  def getSourceConfig(name: String): SourceConfig =
+    SourceConfig(name, this)
 
   def getSinkConfig(name: String): SinkConfig = SinkConfig(name, this)
 
@@ -170,19 +206,21 @@ class FlinkConfig(
     else
       Try(getStringList("source.names")) match {
         case Success(sn) => sn
-        case Failure(_) => getObject("sources").unwrapped().keySet().asScala.toSeq
+        case Failure(_)  =>
+          getObject("sources").unwrapped().keySet().asScala.toSeq
       }
 
   def getSinkNames: Seq[String] =
     Try(getStringList("sink.names")) match {
       case Success(sn) => sn
-      case Failure(_) => getObject("sinks").unwrapped().keySet().asScala.toSeq
+      case Failure(_)  =>
+        getObject("sinks").unwrapped().keySet().asScala.toSeq
     }
 
   lazy val environment = getString("environment")
-  lazy val isDev = environment.startsWith("dev")
-  lazy val isStage = environment.startsWith("stag")
-  lazy val isProd = environment.startsWith("prod")
+  lazy val isDev       = environment.startsWith("dev")
+  lazy val isStage     = environment.startsWith("stag")
+  lazy val isProd      = environment.startsWith("prod")
 
   def configureStreamExecutionEnvironment: SEE = {
     val env =
@@ -201,15 +239,22 @@ class FlinkConfig(
     if (checkpointInterval > 0) {
       env.enableCheckpointing(checkpointInterval)
 
-      env.getCheckpointConfig.setMinPauseBetweenCheckpoints(checkpointMinPause.toMillis)
+      env.getCheckpointConfig.setMinPauseBetweenCheckpoints(
+        checkpointMinPause.toMillis
+      )
 
-      env.getCheckpointConfig.setMaxConcurrentCheckpoints(checkpointMaxConcurrent)
+      env.getCheckpointConfig.setMaxConcurrentCheckpoints(
+        checkpointMaxConcurrent
+      )
 
       val backend = if (stateBackend == "rocksdb") {
         logger.info(s"Using ROCKS DB state backend at $checkpointUrl")
-        val rocksBackend = new RocksDBStateBackend(checkpointUrl, checkpointIncremental)
+        val rocksBackend =
+          new RocksDBStateBackend(checkpointUrl, checkpointIncremental)
         if (checkpointFlash)
-          rocksBackend.setPredefinedOptions(PredefinedOptions.FLASH_SSD_OPTIMIZED)
+          rocksBackend.setPredefinedOptions(
+            PredefinedOptions.FLASH_SSD_OPTIMIZED
+          )
         rocksBackend
       } else {
         logger.info(s"Using FILE SYSTEM state backend at $checkpointUrl")
@@ -224,28 +269,32 @@ class FlinkConfig(
   }
 
   lazy val timeCharacteristic = {
-    getString("time.characteristic").toLowerCase.replaceFirst("\\s*time$", "") match {
-      case "event" => TimeCharacteristic.EventTime
+    getString("time.characteristic").toLowerCase
+      .replaceFirst("\\s*time$", "") match {
+      case "event"      => TimeCharacteristic.EventTime
       case "processing" => TimeCharacteristic.ProcessingTime
-      case "ingestion" => TimeCharacteristic.IngestionTime
-      case unknown => throw new RuntimeException(s"Unknown time.characteristic setting: '$unknown'")
+      case "ingestion"  => TimeCharacteristic.IngestionTime
+      case unknown      =>
+        throw new RuntimeException(
+          s"Unknown time.characteristic setting: '$unknown'"
+        )
     }
   }
 
-  lazy val systemHelp = _config.getString("system.help")
-  lazy val jobHelp = getString("help")
-  lazy val jobDescription = getString("description")
-  lazy val globalParallelism = getInt("global.parallelism")
-  lazy val checkpointInterval = getLong("checkpoint.interval")
-  lazy val checkpointMinPause = getDuration("checkpoint.min.pause")
+  lazy val systemHelp              = _config.getString("system.help")
+  lazy val jobHelp                 = getString("help")
+  lazy val jobDescription          = getString("description")
+  lazy val globalParallelism       = getInt("global.parallelism")
+  lazy val checkpointInterval      = getLong("checkpoint.interval")
+  lazy val checkpointMinPause      = getDuration("checkpoint.min.pause")
   lazy val checkpointMaxConcurrent = getInt("checkpoint.max.concurrent")
-  lazy val checkpointUrl = getString("checkpoint.url")
-  lazy val checkpointFlash = getBoolean("checkpoint.flash")
-  lazy val stateBackend = getString("state.backend").toLowerCase
-  lazy val checkpointIncremental = getBoolean("checkpoint.incremental")
-  lazy val showPlan = getBoolean("show.plan")
-  lazy val mockEdges = isDev && getBoolean("mock.edges")
-  lazy val maxLateness = getDuration("max.lateness")
-  lazy val maxIdleness = getDuration("max.idleness")
+  lazy val checkpointUrl           = getString("checkpoint.url")
+  lazy val checkpointFlash         = getBoolean("checkpoint.flash")
+  lazy val stateBackend            = getString("state.backend").toLowerCase
+  lazy val checkpointIncremental   = getBoolean("checkpoint.incremental")
+  lazy val showPlan                = getBoolean("show.plan")
+  lazy val mockEdges               = isDev && getBoolean("mock.edges")
+  lazy val maxLateness             = getDuration("max.lateness")
+  lazy val maxIdleness             = getDuration("max.idleness")
 
 }
