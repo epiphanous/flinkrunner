207375a6b78ad107bfd45caded40ee5a8db544a0
Robert Lyons <nextdude@gmail.com>
2022-08-29 18:47:00 -0400

    reinstate getSource and getStreamFormat methods

--- src/main/scala/io/epiphanous/flinkrunner/model/source/FileSourceConfig.scala
+++ src/main/scala/io/epiphanous/flinkrunner/model/source/FileSourceConfig.scala
@@ -1,7 +1,12 @@
 package io.epiphanous.flinkrunner.model.source
 
 import io.epiphanous.flinkrunner.model._
-import io.epiphanous.flinkrunner.serde.{DelimitedConfig, DelimitedRowDecoder, JsonRowDecoder, RowDecoder}
+import io.epiphanous.flinkrunner.serde.{
+  DelimitedConfig,
+  DelimitedRowDecoder,
+  JsonRowDecoder,
+  RowDecoder
+}
 import io.epiphanous.flinkrunner.util.FileUtils.getResourceOrFile
 import org.apache.avro.generic.GenericRecord
 import org.apache.flink.api.common.eventtime.WatermarkStrategy
@@ -10,10 +15,16 @@ import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.connector.source.{Source, SourceSplit}
 import org.apache.flink.api.scala.createTypeInformation
 import org.apache.flink.connector.file.src.FileSource
-import org.apache.flink.connector.file.src.reader.{StreamFormat, TextLineInputFormat}
+import org.apache.flink.connector.file.src.reader.{
+  StreamFormat,
+  TextLineInputFormat
+}
 import org.apache.flink.core.fs.Path
 import org.apache.flink.streaming.api.functions.source.SourceFunction
-import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.scala.{
+  DataStream,
+  StreamExecutionEnvironment
+}
 import org.apache.flink.util.Collector
 
 import java.time.Duration
@@ -34,41 +45,7 @@ case class FileSourceConfig[ADT <: FlinkEvent](
     .getProperty("monitor.continuously", "0")
     .toLong
 
-//  def getStreamFormat[E <: ADT: TypeInformation]: StreamFormat[E] =
-//    format match {
-//      case StreamFormatName.Csv       => getCsvStreamFormat
-//      case StreamFormatName.Tsv       => getTsvStreamFormat
-//      case StreamFormatName.Psv       => getPsvStreamFormat
-//      case StreamFormatName.Delimited =>
-//        getDelimitedStreamFormat(DelimitedConfig.get(format, properties))
-//      case _                          =>
-//        throw new RuntimeException(
-//          s"oops, ${format.entryName} files not handled by getStreamFormat for FileSource"
-//        )
-//    }
-
-//  def getCsvStreamFormat[E <: ADT: TypeInformation]: StreamFormat[E] =
-//    getDelimitedStreamFormat(DelimitedConfig.CSV)
-//  def getTsvStreamFormat[E <: ADT: TypeInformation]: StreamFormat[E] =
-//    getDelimitedStreamFormat(DelimitedConfig.TSV)
-//  def getPsvStreamFormat[E <: ADT: TypeInformation]: StreamFormat[E] =
-//    getDelimitedStreamFormat(DelimitedConfig.PSV)
-//  def getDelimitedStreamFormat[E <: ADT: TypeInformation](
-//      c: DelimitedConfig): StreamFormat[E] = {
-//    val ti     = implicitly[TypeInformation[E]]
-//    val mapper = new CsvMapper()
-//
-//    mapper.registerModule(DefaultScalaModule)
-//    val schema: CsvSchema = mapper
-//      .schemaFor(ti.getTypeClass)
-//      .withColumnSeparator(c.columnSeparator)
-//      .withQuoteChar(c.quoteCharacter)
-//      .withEscapeChar(c.escapeChar)
-//      .withLineSeparator(c.lineSeparator)
-//
-//    CsvReaderFormat
-//      .forSchema[E](mapper, schema, ti)
-//  }
+  def getStreamFormat[E <: ADT: TypeInformation]: StreamFormat[E] = ???
 
   def getRowDecoder[E <: ADT: TypeInformation]: RowDecoder[E] =
     format match {
@@ -77,16 +54,16 @@ case class FileSourceConfig[ADT <: FlinkEvent](
         new DelimitedRowDecoder[E](DelimitedConfig.get(format, properties))
     }
 
-//  override def getSource[E <: ADT: TypeInformation]
-//      : Either[SourceFunction[E], Source[E, _ <: SourceSplit, _]] = {
-//    val fsb =
-//      FileSource.forRecordStreamFormat(getStreamFormat, origin)
-//    Right(
-//      (if (monitorDuration > 0)
-//         fsb.monitorContinuously(Duration.ofSeconds(monitorDuration))
-//       else fsb).build()
-//    )
-//  }
+  override def getSource[E <: ADT: TypeInformation]
+      : Either[SourceFunction[E], Source[E, _ <: SourceSplit, _]] = {
+    val fsb =
+      FileSource.forRecordStreamFormat(getStreamFormat, origin)
+    Right(
+      (if (monitorDuration > 0)
+         fsb.monitorContinuously(Duration.ofSeconds(monitorDuration))
+       else fsb).build()
+    )
+  }
 
   override def getSourceStream[E <: ADT: TypeInformation](
       env: StreamExecutionEnvironment): DataStream[E] = {
