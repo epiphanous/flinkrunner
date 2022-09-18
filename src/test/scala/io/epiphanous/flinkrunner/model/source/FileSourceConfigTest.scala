1ab1b1a9214ea94e610328a7807ff9a7db093089
Robert Lyons <nextdude@gmail.com>
2022-09-18 17:42:54 -0400

    added avro file source tests

--- src/test/scala/io/epiphanous/flinkrunner/model/source/FileSourceConfigTest.scala
+++ src/test/scala/io/epiphanous/flinkrunner/model/source/FileSourceConfigTest.scala
@@ -2,6 +2,7 @@ package io.epiphanous.flinkrunner.model.source
 
 import io.epiphanous.flinkrunner.flink.AvroStreamJob
 import io.epiphanous.flinkrunner.model.{
+  AvroFileTestUtils,
   BRecord,
   BWrapper,
   CheckResults,
@@ -11,16 +12,21 @@ import io.epiphanous.flinkrunner.{FlinkRunner, PropSpec}
 import org.apache.flink.api.scala.createTypeInformation
 import org.apache.flink.streaming.api.scala.DataStream
 
-class FileSourceConfigTest extends PropSpec {
+import java.nio.file.{Files, Paths}
+import scala.util.Try
 
-  class TestCheckResults extends CheckResults[MyAvroADT] {
+class FileSourceConfigTest extends PropSpec with AvroFileTestUtils {
+
+  class TestCheckResults(pop: List[BWrapper], path: java.nio.file.Path)
+      extends CheckResults[MyAvroADT] {
     override val name: String = "test-check-results"
 
     override def checkOutputEvents[OUT <: MyAvroADT](
         out: List[OUT]): Unit = {
-//      println(out.map(_.toString).mkString("\n"))
-      out.length shouldEqual 20
-      out.head.asInstanceOf[BWrapper].$record.b0 shouldEqual "lmjr"
+      println("=====[INPUT]======\n" + pop.mkString("\n"))
+      println("\n=====[OUTPUT]======\n" + out.mkString("\n"))
+      Try(Files.delete(path)) should be a 'success
+      out shouldEqual pop
       ()
     }
   }
@@ -31,26 +37,41 @@ class FileSourceConfigTest extends PropSpec {
       singleAvroSource[BWrapper, BRecord]()
   }
 
-  ignore("getAvroSourceStream Avro format property") {
-    val optConfig =
-      """
-        |execution.runtime-mode = batch
-        |jobs {
-        |  test-job {
-        |    sources {
-        |      test-file-source {
-        |        path = "resource://avro-test/"
-        |        format = avro
-        |      }
-        |    }
-        |  }
-        |}
-        |""".stripMargin
-    getAvroJobRunner[TestIdentityJob, BWrapper, BRecord, MyAvroADT](
-      Array("test-job"),
-      optConfig,
-      new TestCheckResults,
-      (_, runner) => new TestIdentityJob(runner)
-    ).process()
+  def doAvroFileSourceTest(isParquet: Boolean) = {
+    val format = if (isParquet) "parquet" else "avro"
+    val pop    = genPop[BWrapper]()
+    getTempFile(isParquet).map { path =>
+      val file      = path.toString
+      writeFile(file, isParquet, pop)
+      val optConfig =
+        s"""
+           |execution.runtime-mode = batch
+           |jobs {
+           |  $format-test-job {
+           |    sources {
+           |      test-file-source {
+           |        path = "$file"
+           |        format = $format
+           |      }
+           |    }
+           |  }
+           |}
+           |""".stripMargin
+      getAvroJobRunner[TestIdentityJob, BWrapper, BRecord, MyAvroADT](
+        Array(s"$format-test-job"),
+        optConfig,
+        new TestCheckResults(pop, path),
+        (_, runner) => new TestIdentityJob(runner)
+      ).process()
+    }
   }
+
+  property("getAvroSourceStream Avro format property") {
+    doAvroFileSourceTest(false)
+  }
+
+  property("getAvroSourceStream Parquet format property") {
+    doAvroFileSourceTest(true)
+  }
+
 }
