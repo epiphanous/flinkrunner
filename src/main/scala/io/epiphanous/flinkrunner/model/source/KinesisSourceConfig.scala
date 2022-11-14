ea74b44e8b2b697192a1bb432e9dee67c48c57c4
Robert Lyons <nextdude@gmail.com>
2022-11-14 06:20:59 -0500

    improve kinesis source config (#54)
    * improve kinesis source config
    
    * clean up
    
    * clean up constants

--- src/main/scala/io/epiphanous/flinkrunner/model/source/KinesisSourceConfig.scala
+++ src/main/scala/io/epiphanous/flinkrunner/model/source/KinesisSourceConfig.scala
@@ -1,20 +1,22 @@
 package io.epiphanous.flinkrunner.model.source
 
+import com.amazonaws.regions.Regions
 import io.epiphanous.flinkrunner.model.{
   FlinkConfig,
   FlinkConnectorName,
   FlinkEvent
 }
 import io.epiphanous.flinkrunner.serde.JsonKinesisDeserializationSchema
-import io.epiphanous.flinkrunner.util.ConfigToProps
 import io.epiphanous.flinkrunner.util.ConfigToProps.getFromEither
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.connector.source.{Source, SourceSplit}
+import org.apache.flink.kinesis.shaded.org.apache.flink.connector.aws.config.AWSConfigConstants.AWS_REGION
 import org.apache.flink.streaming.api.functions.source.SourceFunction
 import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer
+import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants._
 import org.apache.flink.streaming.connectors.kinesis.serialization.KinesisDeserializationSchema
 
-import java.util.Properties
+import scala.util.Try
 
 /** A source config for kinesis streams. For example, the following config
   * can be used to read from a topic in kafka that contains confluent avro
@@ -35,16 +37,15 @@ import java.util.Properties
   *     the source name)
   *   - `stream`: the name of the kinesis stream
   *   - `starting.position`: the starting position of the stream; one of:
-  *     - `AT_SEQUENCE_NUMBER`: at the `starting.sequence` number
-  *     - `AFTER_SEQUENCE_NUMBER`: just after the `starting.sequence`
-  *       number
-  *     - `AT_TIMESTAMP`: on or after the `starting.timestamp`
   *     - `TRIM_HORIZON`: the position of the earliest data in a shard
   *     - `LATEST`: the position after the most recent data in a shard
-  *   - `starting.sequence`: a sequence number in a shard
+  *     - `AT_TIMESTAMP`: on or after the `starting.timestamp`
   *   - `starting.timestamp`: a timestamp as fractional epoch seconds
+  *     (format: `yyyy-MM-dd'T'HH:mm:ss.SSSXXX`)
   *   - `use.efo`: if true, turn on enhanced fan-out to read the stream
   *     faster (defaults to true, may cost more money)
+  *   - `efo.consumer`: name of the efo consumer (defaults to
+  *     `jobName`.`sourceName`)
   *   - `aws.region`: AWS region of your kinesis endpoint
   *   - `config`: optional config to pass to kinesis client
   *
@@ -62,68 +63,88 @@ case class KinesisSourceConfig[ADT <: FlinkEvent](
 
   override val connector: FlinkConnectorName = FlinkConnectorName.Kinesis
 
-  override val properties: Properties = ConfigToProps.normalizeProps(
-    config,
-    pfx(),
-    List("aws.region")
+  val awsRegion: String =
+    getFromEither(pfx(), Seq(AWS_REGION), config.getStringOpt).getOrElse(
+      Regions.US_EAST_1.name()
+    )
+  properties.setProperty(AWS_REGION, awsRegion)
+
+  val stream: String = Try(config.getString(pfx("stream"))).fold(
+    t =>
+      throw new RuntimeException(
+        s"kinesis source $name is missing required 'stream' property",
+        t
+      ),
+    s => s
   )
 
-  val stream: String = config.getString(pfx("stream"))
+  val startPos: String = {
+    val pos = getFromEither(
+      pfx(),
+      Seq(
+        "starting.position",
+        "starting.pos",
+        "start.position",
+        "start.pos",
+        "flink.stream.initpos"
+      ),
+      config.getStringOpt
+    ).getOrElse(InitialPosition.LATEST.name()).toUpperCase
 
-  val startPos: String = getFromEither(
-    pfx(),
-    Seq("starting.position", "flink.stream.initpos"),
-    config.getStringOpt
-  ).getOrElse("TRIM_HORIZON")
+    Try(InitialPosition.valueOf(pos)).fold(
+      t =>
+        throw new RuntimeException(
+          s"Kinesis source $name has invalid `starting.position` <$pos>. Instead, use one of ${InitialPosition.values().map(_.name()).mkString(", ")}",
+          t
+        ),
+      sit => sit.name()
+    )
+  }
 
   val startTimestampOpt: Option[String] = getFromEither(
     pfx(),
-    Seq("starting.timestamp", "starting.ts"),
+    Seq(
+      "starting.timestamp",
+      "starting.ts",
+      "start.timestamp",
+      "start.ts"
+    ),
     config.getStringOpt
   )
 
-  val startSeqNoOpt: Option[String] = getFromEither(
-    pfx(),
-    Seq("starting.sequence", "starting.seq"),
-    config.getStringOpt
-  )
-
-  properties.setProperty("flink.stream.initpos", startPos)
-  (startPos, startTimestampOpt, startSeqNoOpt) match {
-    case ("AT_TIMESTAMP", Some(ts), _) =>
-      properties.setProperty("flink.stream.timestamp", ts)
-    case ("AT_TIMESTAMP", None, _)     =>
+  properties.setProperty(STREAM_INITIAL_POSITION, startPos)
+  if (
+    startPos.equalsIgnoreCase(
+      InitialPosition.AT_TIMESTAMP.name()
+    )
+  ) {
+    startTimestampOpt.fold(
       throw new RuntimeException(
         s"kinesis sink $name set starting.position to AT_TIMESTAMP but provided no starting.timestamp"
       )
-    case (
-          "AT_SEQUENCE_NUMBER" | "AFTER_SEQUENCE_NUMBER",
-          _,
-          Some(seqNo)
-        ) =>
-      properties.setProperty("flink.stream.sequence.number", seqNo)
-    case (
-          "AT_SEQUENCE_NUMBER" | "AFTER_SEQUENCE_NUMBER",
-          _,
-          None
-        ) =>
-      throw new RuntimeException(
-        s"kinesis sink $name set starting.position to $startPos but provided no starting.sequence"
-      )
-    case _                             => // noop
+    )(ts => properties.setProperty(STREAM_INITIAL_TIMESTAMP, ts))
   }
 
   val useEfo: Boolean =
-    getFromEither(pfx(), Seq("use.efo"), config.getBooleanOpt).getOrElse(
-      true
+    getFromEither(
+      pfx(),
+      Seq("use.efo", "efo.enabled"),
+      config.getBooleanOpt
+    ).getOrElse(
+      properties
+        .getProperty(RECORD_PUBLISHER_TYPE, "EFO")
+        .equalsIgnoreCase("EFO")
     )
 
+  val efoConsumer: String = getFromEither(
+    pfx(),
+    Seq("efo.consumer", "flink.stream.efo.consumer", EFO_CONSUMER_NAME),
+    config.getStringOpt
+  ).getOrElse(s"${config.jobName}.$name")
+
   if (useEfo) {
-    properties.setProperty("flink.stream.recordpublisher", "EFO")
-    properties.setProperty(
-      "flink.stream.efo.consumer",
-      s"${config.jobName}.$name"
-    )
+    properties.setProperty(RECORD_PUBLISHER_TYPE, "EFO")
+    properties.setProperty(EFO_CONSUMER_NAME, efoConsumer)
   }
 
   /** Returns a deserialization schema for kinesis. This implementation
