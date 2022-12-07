6f2b954248f9bc3637fda61b27d5ab42a6539eb7
Logan Donoughe <logan.donoughe@3ds.com>
2022-12-07 11:19:15 -0800

    Fix instant to string issue for S3

--- src/main/scala/io/epiphanous/flinkrunner/util/InstantUtils.scala
+++ src/main/scala/io/epiphanous/flinkrunner/util/InstantUtils.scala
@@ -1,12 +1,12 @@
 package io.epiphanous.flinkrunner.util
 
-import java.time.Instant
+import java.time.{Instant, ZoneOffset}
 import java.time.format.DateTimeFormatter
 
 object InstantUtils {
 
   val dtf: DateTimeFormatter =
-    DateTimeFormatter.ofPattern("/yyyy/MM/dd/HH")
+    DateTimeFormatter.ofPattern("/yyyy/MM/dd/HH").withZone(ZoneOffset.UTC)
 
   implicit class RichInstant(instant: Instant) {
     def prefixedTimePath(prefix: String): String =
