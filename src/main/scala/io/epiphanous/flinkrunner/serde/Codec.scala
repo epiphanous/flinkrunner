364559a0a8e6fdb23d4d81160c72e4069b3adf04
Robert Balaban <rbalaban@mdsol.com>
2022-12-07 18:07:56 +0200

    fix: revert merge

--- src/main/scala/io/epiphanous/flinkrunner/serde/Codec.scala
+++ src/main/scala/io/epiphanous/flinkrunner/serde/Codec.scala
@@ -52,10 +52,6 @@ case class Codec[E](
         jsonConfig.sortKeys
       )
       .configure(SerializationFeature.INDENT_OUTPUT, jsonConfig.pretty)
-      .configure(
-        DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE,
-        false
-      )
     (if (isAvro) mapper.addModule(avroModule) else mapper)
       .build() :: ScalaReflectExtensions
   }
