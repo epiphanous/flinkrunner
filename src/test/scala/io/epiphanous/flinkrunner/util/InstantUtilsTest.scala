5bb1bfcf67839750aed6bccf604f52a9ac71d813
Logan Donoughe <logan.donoughe@3ds.com>
2022-12-08 15:10:55 -0800

    Formatting, import optimizing, removal of misleading with clause

--- src/test/scala/io/epiphanous/flinkrunner/util/InstantUtilsTest.scala
+++ src/test/scala/io/epiphanous/flinkrunner/util/InstantUtilsTest.scala
@@ -1,23 +1,22 @@
 package io.epiphanous.flinkrunner.util
 
 import io.epiphanous.flinkrunner.PropSpec
-
-import java.time.Instant
 import io.epiphanous.flinkrunner.util.InstantUtils.RichInstant
 
+import java.time.Instant
 
-class InstantUtilsTest extends PropSpec with StringUtils {
+class InstantUtilsTest extends PropSpec {
 
   property("Adds Prefix") {
-    val prefix = "prefix"
-    val zero_time         =  RichInstant(Instant.ofEpochSecond(0))
+    val prefix           = "prefix"
+    val zero_time        = RichInstant(Instant.ofEpochSecond(0))
     val formatted_string = "prefix/1970/01/01/00"
     zero_time.prefixedTimePath(prefix) shouldEqual formatted_string
   }
 
   property("does not need a prefix") {
-    val prefix = ""
-    val zero_time = RichInstant(Instant.ofEpochSecond(0))
+    val prefix           = ""
+    val zero_time        = RichInstant(Instant.ofEpochSecond(0))
     val formatted_string = "/1970/01/01/00"
     zero_time.prefixedTimePath(prefix) shouldEqual formatted_string
   }
