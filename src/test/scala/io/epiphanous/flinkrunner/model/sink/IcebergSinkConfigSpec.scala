61d20cceb82ccfa9d996cd82b8f3cf073d58a120
Robert Lyons <nextdude@gmail.com>
2023-02-25 08:57:49 -0500

    got basic test working

--- src/test/scala/io/epiphanous/flinkrunner/model/sink/IcebergSinkConfigSpec.scala
+++ src/test/scala/io/epiphanous/flinkrunner/model/sink/IcebergSinkConfigSpec.scala
@@ -11,12 +11,13 @@ import io.epiphanous.flinkrunner.model.{BRecord, MyAvroADT}
 import io.epiphanous.flinkrunner.util.AvroUtils.rowTypeOf
 import org.apache.flink.api.scala.createTypeInformation
 import org.apache.flink.table.types.logical.RowType
+import org.testcontainers.containers.Network
 import org.testcontainers.containers.localstack.LocalStackContainer.Service
+import org.testcontainers.utility.Base58
 import requests.Response
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.{
   CreateBucketRequest,
-  ListObjectsRequest,
   ListObjectsV2Request
 }
 
@@ -26,16 +27,22 @@ class IcebergSinkConfigSpec extends PropSpec with TestContainersForAll {
 
   val bucketName      = "warehouse"
   val icebergRESTPort = 8181
+  val icebergHost     = s"iceberg-${Base58.randomString(6)}"
+  val localstackHost  = s"localstack-${Base58.randomString(6)}"
 
   val icebergImage: GenericContainer.DockerImage =
-    GenericContainer.stringToDockerImage("tabulario/iceberg-rest:latest")
+    GenericContainer.stringToDockerImage("tabulario/iceberg-rest:0.2.0")
 
   override type Containers = LocalStackV2Container and GenericContainer
 
   override def startContainers(): Containers = {
-    val localstack     = LocalStackV2Container
-      .Def(tag = "1.4.0", services = Seq(Service.S3))
-      .start()
+    val network    = Network.newNetwork()
+    val localstack = LocalStackV2Container(
+      tag = "1.4.0",
+      services = Seq(Service.S3)
+    ).configure(_.withNetwork(network).withNetworkAliases(localstackHost))
+    localstack.start()
+
     val creds          = localstack.staticCredentialsProvider.resolveCredentials()
     val env            = Map(
       "CATALOG_S3_ACCESS__KEY__ID"     -> creds.accessKeyId(),
@@ -43,21 +50,22 @@ class IcebergSinkConfigSpec extends PropSpec with TestContainersForAll {
       "AWS_REGION"                     -> localstack.region.toString,
       "CATALOG_WAREHOUSE"              -> s"s3://$bucketName/",
       "CATALOG_IO__IMPL"               -> "org.apache.iceberg.aws.s3.S3FileIO",
-      "CATALOG_S3_ENDPOINT"            -> s3Endpoint(localstack),
+      "CATALOG_S3_ENDPOINT"            -> s3Endpoint(localstack, outside = false),
       "CATALOG_S3_PATH__STYLE__ACCESS" -> "true"
     )
-    val icebergCatalog = GenericContainer
-      .Def(
-        dockerImage = icebergImage,
-        exposedPorts = Seq(icebergRESTPort),
-        env = env
-      )
-      .start()
+    logger.debug(env.toString)
+    val icebergCatalog = GenericContainer(
+      dockerImage = icebergImage,
+      exposedPorts = Seq(icebergRESTPort),
+      env = env
+    ).configure { c =>
+      c.withNetwork(network)
+      c.withNetworkAliases(icebergHost)
+    }
+    icebergCatalog.start()
     localstack and icebergCatalog
   }
 
-  import org.apache.iceberg.jdbc.JdbcCatalog
-  import org.apache.iceberg.aws.s3.S3FileIO
   override def afterContainersStart(containers: Containers): Unit = {
     super.afterContainersStart(containers)
     containers match {
@@ -79,12 +87,20 @@ class IcebergSinkConfigSpec extends PropSpec with TestContainersForAll {
       .build()
   }
 
-  def s3Endpoint(ls: LocalStackV2Container): String =
-    ls.endpointOverride(Service.S3).toString
+  def s3Endpoint(
+      ls: LocalStackV2Container,
+      outside: Boolean = true): String = {
+    if (outside) ls.endpointOverride(Service.S3).toString
+    else
+      s"http://$localstackHost:4566"
+  }
 
-  def icebergRestEndpoint(ib: GenericContainer): String = {
+  def icebergEndpoint(
+      ib: GenericContainer,
+      outside: Boolean = true): String = {
     val mappedPort = ib.container.getMappedPort(icebergRESTPort)
-    s"http://localhost:$mappedPort"
+    if (outside) s"http://localhost:$mappedPort"
+    else s"http://$icebergHost:$mappedPort"
   }
 
   def icebergRest(
@@ -92,7 +108,7 @@ class IcebergSinkConfigSpec extends PropSpec with TestContainersForAll {
       path: String,
       params: Iterable[(String, String)] = Nil): Response = {
     val endpoint =
-      s"${icebergRestEndpoint(ib)}${if (path.startsWith("/")) ""
+      s"${icebergEndpoint(ib)}${if (path.startsWith("/")) ""
         else "/"}$path"
     requests.get(endpoint, params = params)
   }
@@ -101,8 +117,6 @@ class IcebergSinkConfigSpec extends PropSpec with TestContainersForAll {
       ls: LocalStackV2Container,
       ib: GenericContainer,
       rowType: RowType): Unit = {
-    logger.debug(s"iceberg rest endpoint: ${icebergRestEndpoint(ib)}")
-    logger.debug(s"s3 endpoint: ${s3Endpoint(ls)}")
     val runner     = getRunner[MyAvroADT](
       Array.empty[String],
       Some(s"""
@@ -111,9 +125,9 @@ class IcebergSinkConfigSpec extends PropSpec with TestContainersForAll {
            |    connector = "iceberg"
            |    catalog {
            |      name = iceberg
-           |      uri = "${icebergRestEndpoint(ib)}"
+           |      uri = "${icebergEndpoint(ib)}"
            |      io-impl = "org.apache.iceberg.aws.s3.S3FileIO"
-           |      s3.endpoint = "${s3Endpoint(ls)}"
+           |      s3.endpoint = "${s3Endpoint(ls, outside = false)}"
            |      warehouse = "s3://$bucketName"
            |    }
            |    namespace = "testing.tables"
